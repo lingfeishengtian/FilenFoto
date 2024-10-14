ccd5d882846c307a878fe42f8b4fcec02103d913
Hunter Han <hunter.han@gmail.com>
2024-10-14 12:34:41 -0500

    Initial Commit

---
+++ FilenFoto/FilenFotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilenFotoApp.swift
+//  FilenFoto
+//
+//  Created by Hunter Han on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FilenFotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FilenFotoTests/FilenFotoTests.swift
@@ -0,0 +1,16 @@
+//
+//  FilenFotoTests.swift
+//  FilenFotoTests
+//
+//  Created by Hunter Han on 10/14/24.
+//
+
+import Testing
+
+struct FilenFotoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
