85b2ef70f7ffa0612008f72925c8bba74a57efe1
Hunter Han <hunter.han@gmail.com>
2024-11-09 18:37:26 -0600

    Code cleanup

---
+++ FilenFoto/HelperViews/SearchResultsGrid.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsGrid.swift
+//  FilenFoto
+//
+//  Created by Hunter Han on 11/8/24.
+//
+
+import SwiftUI
+
+struct SearchResultsGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResultsGrid()
+}
---
+++ FilenFoto/Persistence/DBPhotoAsset.swift
@@ -0,0 +1,8 @@
+//
+//  DBPhotoAsste.swift
+//  FilenFoto
+//
+//  Created by Hunter Han on 11/9/24.
+//
+
+import Foundation
